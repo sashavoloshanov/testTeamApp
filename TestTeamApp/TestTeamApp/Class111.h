ad3e24f0eaa016471d764ab726fd0780ffd90f61
sashavoloshanov <svoloshanov@icloud.com>
2016-01-02 23:50:57 +0200

    2121212

---
+++ TestTeamApp/TestTeamApp/Class111.h
@@ -0,0 +1,13 @@
+//
+//  Class111.h
+//  TestTeamApp
+//
+//  Created by Voloshanov Sasha on 1/2/16.
+//  Copyright © 2016 Sasha Voloshanov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Class111 : UIView
+
+@end
---
+++ TestTeamApp/TestTeamApp/Class111.m
@@ -0,0 +1,21 @@
+//
+//  Class111.m
+//  TestTeamApp
+//
+//  Created by Voloshanov Sasha on 1/2/16.
+//  Copyright © 2016 Sasha Voloshanov. All rights reserved.
+//
+
+#import "Class111.h"
+
+@implementation Class111
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
