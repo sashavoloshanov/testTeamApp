e2e0254e53296d9a8cfccaf337e153c2cabaf36e
sashavoloshanov <svoloshanov@icloud.com>
2016-01-02 23:20:14 +0200

    еуіе

---
+++ TestTeamApp/TestTeamApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestTeamApp
+//
+//  Created by Voloshanov Sasha on 1/2/16.
+//  Copyright © 2016 Sasha Voloshanov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestTeamApp/TestTeamApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestTeamApp
+//
+//  Created by Voloshanov Sasha on 1/2/16.
+//  Copyright © 2016 Sasha Voloshanov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
