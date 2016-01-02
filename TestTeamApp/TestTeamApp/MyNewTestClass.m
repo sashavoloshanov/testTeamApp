ca8f3597035916b672364aba3beb2209ec816a8f
sashavoloshanov <svoloshanov@icloud.com>
2016-01-02 23:45:24 +0200

    myNewClass

---
+++ TestTeamApp/TestTeamApp/MyNewTestClass.m
@@ -0,0 +1,13 @@
+//
+//  MyNewTestClass.m
+//  TestTeamApp
+//
+//  Created by Voloshanov Sasha on 1/2/16.
+//  Copyright © 2016 Sasha Voloshanov. All rights reserved.
+//
+
+#import "MyNewTestClass.h"
+
+@implementation MyNewTestClass
+
+@end
